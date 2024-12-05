acd75597b20d0eb989fb020ed69e5738bb2a732c
pedrosiufc <pedrosiufc@gmail.com>
2024-12-05 09:44:38 -0300

    Manipulando arrays com useState

--- src/App.js
+++ src/App.js
@@ -1,60 +1,42 @@
 import { useState } from 'react';
 
 function App() { //react baseado em component 
- 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
 
-  const[user, setUser] = useState({}); //começando com um objeto vazio
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
 
-function handleRegister(e){
-  e.preventDefault();
-  
-  alert('Usuário Registrado com Sucesso')
-  setUser({
-    nome: nome,
-    idade: idade,
-    email: email,
-  })
-}
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input])//utilizando o spread operator
+    setInput(''); //limpando a entrada
+  }
 
   return (
     <div>
       <h1>Cadastrando Usuário</h1>
-     <form onSubmit={handleRegister}>
-      <label>Nome</label><br/>
-      <input 
-      placeholder='Digite seu nome'
-      value={nome}
-      onChange={(e)=> setNome(e.target.value) }
-      /><br/>
+      <form onSubmit={handleRegister}>
+        <label>Nome da Tarefa</label><br />
+        <input
+          placeholder='Digite uma tarefa'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        /><br />
 
-      <label>Email</label><br/>
-      <input
-      placeholder='Digite seu Email'
-      value={email}
-      onChange={(e)=> setEmail(e.target.value) }
-      /><br/>
+        <button type="submit">Registrar</button>
+      </form>
 
-      <label>Idade</label><br/>
-      <input 
-      placeholder='Digite sua idade'
-      value={idade}
-      onChange={(e)=> setIdade(e.target.value) }
-      /><br/>
-     
-      <button type="submit">Registrar</button>
-     </form>
-     
-     <br/><br/>
-     
-     <div>
-        <span>Bem vindo: {user.nome}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>Email: {user.email}</span><br/>
+      <br /><br />
 
-     </div>
+      <ul>
+        {tarefas.map(tarefa => ( //utlizando map para percorer a lista
+          <li key={tarefa}>{tarefa}</li> //utlizando kei para evitar erros no console
+        ))}
+      </ul>
 
     </div>
   );
